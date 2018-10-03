2b750cf2e934dc90ba56f6425f6236300fd61df4
Ryan <ryanfurtado.elre@gmail.com>
2018-10-03 15:11:23 -0400

    Added base files
    These are just the base files for the project. They contain the HTML page that will display the list and the JS file that holds the list of fish to display.

---
+++ Animal Crossing Wild World - Fishing Guide/root/js/fishList.js
@@ -0,0 +1,59 @@
+// Animal Crossing: Wild World Fish List
+// This is a list of all fish in AC:WW.
+
+var fishList = [
+    {
+        fishName: "Angelfish",
+        fishLoc: "River",
+        fishTime: "4 p.m. - 9 a.m.",
+        fishAvbl: "May - Oct"
+    },
+    {
+        fishName: "Arapaima",
+        fishLoc: "River",
+        fishTime: "4 p.m. - 9 a.m.",
+        fishAvbl: "Jul - Sept"
+    },
+    {
+        fishName: "Arowana",
+        fishLoc: "River",
+        fishTime: "4 p.m. - 9 a.m.",
+        fishAvbl: "Jun - Sept (15th)"
+    },
+    {
+        fishName: "Barbel Steed",
+        fishLoc: "River",
+        fishTime: "All Day",
+        fishAvbl: "All Year"
+    },
+    {
+        fishName: "Barred Knifejaw",
+        fishLoc: "Ocean",
+        fishTime: "All Day",
+        fishAvbl: "Mar - Nov"
+    },
+    {
+        fishName: "Bitterling",
+        fishLoc: "River",
+        fishTime: "All Day",
+        fishAvbl: "Nov - Mar"
+    },
+    {
+        fishName: "Black Bass",
+        fishLoc: "River",
+        fishTime: "All Day",
+        fishAvbl: "All Year"
+    },
+    {
+        fishName: "Blue Marlin",
+        fishLoc: "Ocean",
+        fishTime: "All Day",
+        fishAvbl: "Jun - Sept"
+    },
+    {
+        fishName: "Bluegill",
+        fishLoc: "River",
+        fishTime: "9 a.m. - 4 p.m.",
+        fishAvbl: "Jun - Sept"
+    }
+]
