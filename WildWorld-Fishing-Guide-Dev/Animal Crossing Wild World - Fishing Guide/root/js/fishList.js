f3e36a858e15ba1603a48d1780878dd5c65eba70
Ryan <ryanfurtado.elre@gmail.com>
2018-11-12 10:40:24 -0500

    File Updates

---
+++ WildWorld-Fishing-Guide-Dev/Animal Crossing Wild World - Fishing Guide/root/js/fishList.js
@@ -0,0 +1,245 @@
+// Animal Crossing: Wild World Fish List
+// This is a list of all fish in AC:WW.
+
+var fishList = [
+    {
+        fishName: "Angelfish",
+        fishLoc: "River",
+        fishTime: "4 p.m. - 9 a.m.",
+        fishAvbl: "May - Oct"
+    },
+    {
+        fishName: "Arapaima",
+        fishLoc: "River",
+        fishTime: "4 p.m. - 9 a.m.",
+        fishAvbl: "Jul - Sept"
+    },
+    {
+        fishName: "Arowana",
+        fishLoc: "River",
+        fishTime: "4 p.m. - 9 a.m.",
+        fishAvbl: "Jun - Sept (15th)"
+    },
+    {
+        fishName: "Barbel Steed",
+        fishLoc: "River",
+        fishTime: "All Day",
+        fishAvbl: "All Year"
+    },
+    {
+        fishName: "Barred Knifejaw",
+        fishLoc: "Ocean",
+        fishTime: "All Day",
+        fishAvbl: "Mar - Nov"
+    },
+    {
+        fishName: "Bitterling",
+        fishLoc: "River",
+        fishTime: "All Day",
+        fishAvbl: "Nov - Mar"
+    },
+    {
+        fishName: "Black Bass",
+        fishLoc: "River",
+        fishTime: "All Day",
+        fishAvbl: "All Year"
+    },
+    {
+        fishName: "Blue Marlin",
+        fishLoc: "Ocean",
+        fishTime: "All Day",
+        fishAvbl: "Jun - Sept"
+    },
+    {
+        fishName: "Bluegill",
+        fishLoc: "River",
+        fishTime: "9 a.m. - 4 p.m.",
+        fishAvbl: "All Year"
+    },
+    {
+        fishName: "Carp",
+        fishLoc: "River",
+        fishTime: "All Day",
+        fishAvbl: "All Year"
+    },
+    {
+        fishName: "Catfish",
+        fishLoc: "Lake",
+        fishTime: "4 p.m. - 9 a.m.",
+        fishAvbl: "May - Oct"
+    },
+    {
+        fishName: "Char",
+        fishLoc: "Waterfall",
+        fishTime: "4 a.m. - 9 a.m. <br> 4 p.m. - 9 p.m.",
+        fishAvbl: "Mar - Jun <br> Sept - Nov"
+    },
+    {
+        fishName: "Cherry Salmon",
+        fishLoc: "River",
+        fishTime: "All Day",
+        fishAvbl: "Mar - Jun <br> Sept - Nov"
+    },
+    {
+        fishName: "Clownfish",
+        fishLoc: "Ocean",
+        fishTime: "All Day",
+        fishAvbl: "Apr - Sept"
+    },
+    {
+        fishName: "Coelacanth",
+        fishLoc: "Ocean <em>(raining)</em>",
+        fishTime: "4 p.m. - 9 a.m.",
+        fishAvbl: "All Year"
+    },
+    {
+        fishName: "Crawfish",
+        fishLoc: "Pond",
+        fishTime: "All Day",
+        fishAvbl: "Apr - Sept (15th)"
+    },
+    {
+        fishName: "Crucian Carp",
+        fishLoc: "River",
+        fishTime: "All Day",
+        fishAvbl: "All Year"
+    },
+    {
+        fishName: "Dab",
+        fishLoc: "Ocean",
+        fishTime: "All Day",
+        fishAvbl: "Oct - Apr"
+    },
+    {
+        fishName: "Dace",
+        fishLoc: "River",
+        fishTime: "4 p.m. - 9 a.m.",
+        fishAvbl: "All Year"
+    },
+    {
+        fishName: "Dorado",
+        fishLoc: "River",
+        fishTime: "4 a.m. - 9 p.m.",
+        fishAvbl: "Jun - Sept"
+    },
+    {
+        fishName: "Eel",
+        fishLoc: "River",
+        fishTime: "4 p.m. - 9 a.m.",
+        fishAvbl: "Jun - Sept (15th)"
+    },
+    {
+        fishName: "Football Fish",
+        fishLoc: "Ocean",
+        fishTime: "4 p.m. - 9 a.m.",
+        fishAvbl: "Nov - Mar"
+    },
+    {
+        fishName: "Freshwater Goby",
+        fishLoc: "River",
+        fishTime: "4 p.m. - 9 a.m.",
+        fishAvbl: "All Year"
+    },
+    {
+        fishName: "Frog",
+        fishLoc: "Pond",
+        fishTime: "All Day",
+        fishAvbl: "May - Aug"
+    },
+    {
+        fishName: "Gar",
+        fishLoc: "Lake",
+        fishTime: "4 p.m. - 9 a.m.",
+        fishAvbl: "Jun - Sept"
+    },
+    {
+        fishName: "Giant Snakehead",
+        fishLoc: "Lake",
+        fishTime: "9 a.m. - 4 p.m.",
+        fishAvbl: "Jun - Aug"
+    },
+    {
+        fishName: "Goldfish",
+        fishLoc: "River",
+        fishTime: "All Day",
+        fishAvbl: "All Year"
+    },
+    {
+        fishName: "Guppy",
+        fishLoc: "River",
+        fishTime: "9 a.m. - 4 p.m.",
+        fishAvbl: "Apr - Nov"
+    },
+    {
+        fishName: "Hammerhead Shark",
+        fishLoc: "Ocean",
+        fishTime: "4 p.m. - 9 a.m.",
+        fishAvbl: "Jun - Sept"
+    },
+    {
+        fishName: "Horse Mackerel",
+        fishLoc: "Ocean",
+        fishTime: "All Day",
+        fishAvbl: "All Year"
+    },
+    {
+        fishName: "Jellyfish",
+        fishLoc: "Ocean",
+        fishTime: "All Day",
+        fishAvbl: "Aug"
+    },
+    {
+        fishName: "Killifish",
+        fishLoc: "Pond",
+        fishTime: "All Day",
+        fishAvbl: "Apr - Aug"
+    },
+    {
+        fishName: "King Salmon",
+        fishLoc: "River Mouth <br> River",
+        fishTime: "All Day",
+        fishAvbl: "Sept (1st - 14th) <br> Sept (15th - 30th)"
+    },
+    {
+        fishName: "Koi",
+        fishLoc: "River",
+        fishTime: "4 p.m. - 9 a.m.",
+        fishAvbl: "All Year"
+    },
+    {
+        fishName: "Loach",
+        fishLoc: "River",
+        fishTime: "All Day",
+        fishAvbl: "Mar - May"
+    },
+    {
+        fishName: "Ocean Sunfish",
+        fishLoc: "Ocean",
+        fishTime: "4 a.m. - 9 p.m.",
+        fishAvbl: "Jun - Sept"
+    },
+    {
+        fishName: "Octopus",
+        fishLoc: "Ocean",
+        fishTime: "All Day",
+        fishAvbl: "Mar - Jul <br> Sept - Jan"
+    },
+    {
+        fishName: "Olive Flounder",
+        fishLoc: "Ocean",
+        fishTime: "All Day",
+        fishAvbl: "All Year"
+    },
+    {
+        fishName: "Pale Chub",
+        fishLoc: "River",
+        fishTime: "9 a.m. - 4 p.m.",
+        fishAvbl: "All Year"
+    },
+    {
+        fishName: "Piranha",
+        fishLoc: "River",
+        fishTime: "9 a.m. - 4 p.m.",
+        fishAvbl: "Jun - Sept (15th)"
+    }
+]
---
+++ WildWorld-Fishing-Guide-Dev/source/notes.md
@@ -0,0 +1,7 @@
+# Notes
+
+## Useful Links
+
+*  **Tables** 
+    * Separate outer border and cell border - https://stackoverflow.com/questions/8889567/how-can-i-have-a-rounded-border-on-my-table-and-border-collapse-collapse
+    * Borders only on the inside (CSS Tricks) - https://css-tricks.com/table-borders-inside/
